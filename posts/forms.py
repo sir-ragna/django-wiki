ca47936b3d467351176da10dbc65c34cad3c4fb0
ragna <sir.ragna@gmail.com>
2021-11-22 22:34:32 +0100

    form styling

--- posts/forms.py
+++ posts/forms.py
@@ -6,16 +6,13 @@ class Post(forms.Form):
         label="title", 
         max_length=100, 
         widget=forms.TextInput(
-            attrs={
-                'class': 'form-control mb-2', 
-                'placeholder': 'Title'
-            }
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Title'}
         )
     )
     content = forms.CharField(
-        max_length=10240, 
+        max_length=10240,
         widget=forms.Textarea(attrs={
-            'class': 'form-control', 
+            'class': 'form-control mb-2', 
             'rows': '10', 
             'placeholder': 'Write your article here'
         })
